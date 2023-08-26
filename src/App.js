4be023d7ffb1d8f3f47ff3279945763bc896981a
mr-vikash <mrvikash700@gmail.com>
2023-08-27 00:00:44 +0530

    learn-useref

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { useEffect, useState,useRef } from 'react';
+import './App.css';
+import Header from './components/Header'
+const App =()=>
+{
+  const [input,setInput] = useState("");
+  // const prevState = useRef("")
+
+  const inputField = useRef();
+
+  // useEffect(()=>{
+  //     // setCounter(counter + 1); At this case we have need to use useRef
+  //     // counter.current = counter.current + 1;
+
+  //     // also use for restore previous state
+  //     prevState.current = input;
+
+  // },[input])
+
+  const changeHandler =(e)=>{
+    setInput(e.target.value);
+  }
+
+  const handleFocus = () =>
+  {
+    inputField.current.focus();
+  }
+  return(
+    <div>
+      <Header/>
+      <input ref={inputField} value={input} onChange={changeHandler}/>
+      <button onClick={()=>{
+        // inputField.current.focus();
+        handleFocus()
+      }}>Click me for focus</button>
+      {/* <h4>One Letter Before Text  is {prevState.current}</h4> */}
+      {/* <h3>Component Rerendered  = {counter.current}</h3> */}
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div style={{background:"violet",textAlign:'center',padding:"10px"}}>
+      <h1 style={{fontSize:"50px"}}>useRef Hook</h1>
+    </div>
+  )
+}
+
+export default Header
